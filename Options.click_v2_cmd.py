9b5eb81f3a311510089d191cb77097322447d205
Mark Nations <45960551+markusj1201@users.noreply.github.com>
2019-05-30 11:11:55 -0500

    Create Options.click_v2_cmd.py

---
+++ Options.click_v2_cmd.py
@@ -0,0 +1,10 @@
+> python caesar_script_v2.py --help
+
+Usage: caesar_script_v2.py [OPTIONS]
+
+Options:
+
+  --input_file FILENAME   # File in which the encrypted/decrypted text will be written. If not provided, the output text will just be printed.
+  -d, --decrypt / -e, --encrypt # Whether you want to encrypt the input text or decrypt it.
+  -k, --key INTEGER # The numeric key to use for the Caesar encryption/decryption.
+  --help  #Show this message and exit.
