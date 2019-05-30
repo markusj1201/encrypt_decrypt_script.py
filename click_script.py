b61bc30edbd3daf29c6f7618e6cf0b48eea88928
Mark Nations <45960551+markusj1201@users.noreply.github.com>
2019-05-30 10:52:39 -0500

    Create click_script.py
    import click to utilize this caesarcipher script

---
+++ click_script.py
@@ -0,0 +1,33 @@
+import click
+
+
+
+from caesar_encryption import encrypt
+
+
+
+@click.command()
+
+@click.argument('text', nargs=-1)
+
+@click.option('--decrypt/--encrypt', '-d/-e')
+
+@click.option('--key', '-k', default=1)
+
+def caesar(text, decrypt, key):
+
+    text_string = ' '.join(text)
+
+    if decrypt:
+
+        key = -key
+
+    cyphertext = encrypt(text_string, key)
+
+    click.echo(cyphertext)
+
+
+
+if __name__ == '__main__':
+
+    caesar()
